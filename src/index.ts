61abbe95ca7b1f700f71ef9d82ea969e47bad581
Ender Wolf <enderwolf006@gmail.com>
2024-11-29 20:13:19 +0800

    volcengine

--- src/index.ts
+++ src/index.ts
@@ -155,15 +155,15 @@ basekit.addField({
       
       for (let i = 0; i < files.length; i++) {
         const url = files[i].tmp_url
-        let api = domain + `/addWatermark?url=${url}&time=${date ? new Date(date).getTime() + 1000 * 60 * 60 * 8 : '@NULL@'}&text=${encodeURIComponent(text[0].text || text[0][0].text)}&direction=${direction.value}&origin_name=${encodeURIComponent(files[i].name)}`;
+        let api = domain + `/addWatermark?url=${url}&time=${date ? new Date(date).getTime() + 1000 * 60 * 60 * 8 : '@NULL@'}&text=${encodeURIComponent(text[0].text || text[0][0].text)}&direction=${direction.value}&origin_name=${encodeURIComponent(files[i].name)}&tenantKey=${context.tenantKey}`;
         
         const data = (await (await context.fetch(api, { method: 'GET' })).json());
         console.log(`服务端返回：`, data);
         
         if (!data || !data['suc']) continue
-        const { fileName, width, height } = data
+        const { imageURL,fileName, width, height } = data
         const info = {
-          content: domain + `/out/${fileName}`,
+          content: imageURL,
           name: fileName,
           contentType: 'attachment/url',
           width,
